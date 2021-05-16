7fe54cea887b579ede0b1fa6cb950c7779c61927
ErmekZhumagulov <erm.zhumagulov@gmail.com>
2021-05-17 03:27:05 +0600

    third commit

--- admin/function.php
+++ admin/function.php
@@ -103,3 +103,35 @@ function writeJSON() {
     }
     mysqli_close($conn);
 }
+
+function loadGoods() {
+    $conn = connect();
+    $sql = "SELECT * FROM goods";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $out = array();
+        while($row = mysqli_fetch_assoc($result)) {
+            $out[$row["id"]] = $row;
+        }
+        echo json_encode($out);
+    } else {
+        echo "0";
+    }
+    mysqli_close($conn);
+}
+
+function loadSingleGoods() {
+    $id = $_POST['id'];
+    $conn = connect();
+    $sql = "SELECT * FROM goods WHERE id='$id'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        echo json_encode($row);
+    } else {
+        echo "0";
+    }
+    mysqli_close($conn);
+}
